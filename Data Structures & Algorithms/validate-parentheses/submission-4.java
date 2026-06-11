9bf8a782dcfe272bef7c5d3e81094c93f1cc43d5
Andrew Shi <42019625+shiandrew@users.noreply.github.com>
2026-06-10 23:40:08 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (map.containsValue(c)) {
+                stack.push(c);
+            } else if (map.containsKey(c)) {
+                if (stack.empty() || stack.pop() != map.get(c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
