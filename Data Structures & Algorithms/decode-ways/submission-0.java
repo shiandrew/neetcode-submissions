c764bb14d66c40a7026029b27c1dd5f64811ce79
Andrew Shi <42019625+shiandrew@users.noreply.github.com>
2026-06-13 15:29:33 -0700

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+            }
+
+            if (i + 1 < s.length() && 
+            (s.charAt(i) == '1' || 
+            (s.charAt(i) == '2' && 
+            "0123456".indexOf(s.charAt(i + 1)) != -1))) {
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+}
