cfcb6cd9c7770d72ffe3a6bddf4863b90143cc27
Andrew Shi <42019625+shiandrew@users.noreply.github.com>
2026-06-13 13:06:56 -0700

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.java
@@ -0,0 +1,36 @@
+class Solution {
+    List<List<Integer>> adj = new ArrayList<>();
+    Set<Integer> visit = new HashSet<>();
+
+    public boolean validTree(int n, int[][] edges) {
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        return dfs(0, -1) && visit.size() == n;
+    }
+
+    private boolean dfs(int node, int parent) {
+        if (visit.contains(node)) {
+            return false;
+        }
+
+        visit.add(node);
+        if (adj.get(node).isEmpty()) {
+            return true;
+        }
+        
+        for (int neighbor : adj.get(node)) {
+            if (neighbor == parent) {
+                continue;
+            }
+            if (!dfs(neighbor, node)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
